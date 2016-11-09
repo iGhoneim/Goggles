60f057d1f5e7368cb329ab65bfcf9fb6c3af439d
iGhoneim <Ghoneim@Outlook.com>
2016-11-09 19:02:24 +0200

    add activity events

--- app/src/main/java/info/Goggles/Home.java
+++ app/src/main/java/info/Goggles/Home.java
@@ -10,4 +10,35 @@ public class Home extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 }
