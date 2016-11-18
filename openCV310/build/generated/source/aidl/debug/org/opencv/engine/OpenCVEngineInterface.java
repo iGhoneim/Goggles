6b80c7726153d0a414b30029e419e99496b671af
Ahmed-hesham <punisherprogrammer@gmail.com>
2016-11-18 23:49:38 +0200

    testing Hesham

--- openCV310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCV310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Faculty\\semester_9\\gradutaiton_projrcet\\smart_glass\\Android\\Goggles\\Goggles\\openCV310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\Ahmed Hesham\\AndroidStudioProjects\\project\\Goggles\\openCV310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
